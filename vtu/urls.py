d957d9bbaf67042c509210e5b38032c633f86a87
morelucks <luckykamshak@gmail.com>
2024-02-24 19:59:59 +0000

    login functions

--- vtu/urls.py
+++ vtu/urls.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home,  name='home'),
-]
+    path('', views.home, name='home' ),
+    path('logout/', views.logout_user, name='logout' ),
+    path('register/', views.register, name='register' ),
+    ]
--- vtu/views.py
+++ vtu/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have login  successfully')
+            return redirect('home')
+        else:
+            messages.success(request, "we couldn't find an account with that usernam, try again")
+            return redirect('home')
+    else:
+        return render(request, 'home.html')
+    
+def logout_user(request):
+    logout(request)
+    messages.success(request, "you've logout successfully")
+    return redirect('home')
+
+def register(request):
+    form = SignUpForm()
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "You have successfully registered!")
+            return redirect('home')
+        else:
+            messages.error(request, "Please enter correct data for registration.")
+            # Optionally, you can pass the invalid form back to the template for display.
+            return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
