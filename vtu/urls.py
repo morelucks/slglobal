804b2b45ca62e0ad2397db0e3ba9eeb755290fad
morelucks <luckykamshak@gmail.com>
2024-03-02 17:02:32 +0000

    modifications

--- vtu/urls.py
+++ vtu/urls.py
@@ -4,6 +4,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home' ),
+    path('dashboard/', views.dashboard, name='dashboard' ),
+    path('login/', views.login, name='login' ),
+
     path('logout/', views.logout_user, name='logout' ),
     path('register/', views.register, name='register' ),
     ]
--- vtu/views.py
+++ vtu/views.py
@@ -4,6 +4,9 @@ from django.contrib import messages
 from .forms import SignUpForm
 # Create your views here.
 def home(request):
+    
+        return render(request, 'home.html')
+def login(request):
     if request.method=="POST":
         username=request.POST['username']
         password=request.POST['password']
@@ -11,17 +14,17 @@ def home(request):
         if user is not None:
             login(request, user)
             messages.success(request, 'You have login  successfully')
-            return redirect('home')
+            return redirect('dashboard')
         else:
             messages.success(request, "we couldn't find an account with that usernam, try again")
-            return redirect('home')
+            return redirect('login')
     else:
-        return render(request, 'home.html')
+      return render(request, 'login.html')
     
 def logout_user(request):
     logout(request)
     messages.success(request, "you've logout successfully")
-    return redirect('home')
+    return redirect('dashboard')
 
 def register(request):
     form = SignUpForm()
@@ -31,10 +34,13 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, "You have successfully registered!")
-            return redirect('home')
+            return redirect('dashboard')
         else:
             messages.error(request, "Please enter correct data for registration.")
             # Optionally, you can pass the invalid form back to the template for display.
             return render(request, 'register.html', {'form': form})
 
     return render(request, 'register.html', {'form': form})
+
+def dashboard(request):
+    return render(request, 'dashbord/dashboard.html')
